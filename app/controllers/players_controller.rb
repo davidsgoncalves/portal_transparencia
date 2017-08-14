915a477efa5ea44382041bcc6f7f0ae1a16950a1
David <davidsg.dev@gmail.com>
2017-08-14 08:43:22 -0300

    finish players crud

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,4 +4,47 @@ class PlayersController < ApplicationController
   def index
     @players = Player.all
   end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    player = Player.new player_params
+    if player.save
+      flash[:info] = "Você criou jogadores com sucesso"
+    else
+      flash[:info] = "Erro: Nome precisa existir e ser diferente de qualquer outro."
+    end
+
+    redirect_to players_path
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+
+    if @player.update player_params
+      flash[:info] = "Você editou com sucesso"
+    end
+
+    redirect_to players_path
+  end
+
+  def destroy
+    if Player.find(params[:id]).delete
+      flash[:info] = "Você deletado com sucesso"
+    end
+
+    redirect_to players_path
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name)
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -10,6 +10,7 @@
 
 class Player < ActiveRecord::Base
   validates_presence_of :name
+  validates_uniqueness_of :name
 
   has_many :payments
 end
